50091f414622708440604fd5db6d9b0f5d49f1cc
munjji <ehowl1117@naver.com>
2024-05-17 22:43:04 +0900

    [feat] 설문조사 장르 버튼 css 수정 및 api 구현

--- moodoodleFront/moodoodlefront/src/components/GenreButton.jsx
+++ moodoodleFront/moodoodlefront/src/components/GenreButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function GenreButton({ genre }) {
+export default function GenreButton({ genre, onClick, isClick }) {
   return (
     <button
+      onClick={onClick}
       type="button"
-      className="flex flex-row justify-center items-center w-[313px] h-[31px] rounded-[30px] bg-[#FFEEF4] shadow-buttonShadow text-[12px] text-darkGray font-normal"
-    >
+      className={`flex flex-row justify-center items-center w-[313px] h-[31px] rounded-[30px] bg-[#FFEEF4] shadow-buttonShadow text-[12px] text-darkGray font-normal ${isClick} hover:border border-[#d6c2c9]`}>
       {genre}
     </button>
   );
--- moodoodleFront/moodoodlefront/src/pages/Survey.jsx
+++ moodoodleFront/moodoodlefront/src/pages/Survey.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import genre from '../constants/genre';
-import GenreButton from '../components/GenreButton';
-import CustomButton from '../components/CustomButton';
+import { Link, useNavigate } from 'react-router-dom';
+import SurveyComponent from '../components/SurveyComponent';
+import useSurvey from '../hooks/useSurvey';
 
 export default function Survey() {
-  const [answers, setAnswers] = useState({ positive: [], negative: [] });
-  const handleSelectedGenre = () => {};
+  const navigate = useNavigate();
+  const [isFirstStep, setIsFirstStep] = useState(true);
+  const [positives, setPositives] = useState([]);
+  const [negatives, setNegatives] = useState([]);
+  const { postSurveyAnswers } = useSurvey();
+
+  const handleFirstStepSubmit = () => {
+    setIsFirstStep((prev) => !prev);
+  };
+
+  const handleFinalStepSubmit = () => {
+    setIsFirstStep((prev) => !prev);
+    postSurveyAnswers(positives, negatives);
+    setPositives([]);
+    setNegatives([]);
+    navigate('/login');
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
@@ -14,21 +28,23 @@ export default function Survey() {
         <Link to="/start">
           <img src="/assets/moodoodleLogoMain.svg" alt="logo" />
         </Link>
-        <div className="flex flex-col w-[283px] h-[73px] text-center">
-          <p className="text-[16px] font-semibold">첫 번째 질문</p>
-          <p className="text-[14px] font-semibold">
-            [긍정]적인 감정일 때 어떤 장르의 음악을 듣나요?
-            <br />
-            (긍정 : 평온, 성취감, 행복, 열정)
-          </p>
-          <p className="text-darkGray text-[12px]">복수 선택 가능</p>
-        </div>
-        <div className="flex flex-col justify-between items-center w-[313px] h-[444px]">
-          {genre.GENRE.map((v) => (
-            <GenreButton genre={v} onClick />
-          ))}
-        </div>
-        <CustomButton text="제출하기" color="purple" />
+        {isFirstStep ? (
+          <SurveyComponent
+            title="첫 번째 질문"
+            direction="긍정"
+            answers={positives}
+            setAnswers={setPositives}
+            handleSubmit={handleFirstStepSubmit}
+          />
+        ) : (
+          <SurveyComponent
+            title="두 번째 질문"
+            direction="부정"
+            answers={negatives}
+            setAnswers={setNegatives}
+            handleSubmit={handleFinalStepSubmit}
+          />
+        )}
       </div>
     </div>
   );
