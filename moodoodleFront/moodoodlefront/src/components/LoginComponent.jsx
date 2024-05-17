18146e2cefeafaf385bc8074e225456c3670688e
munjji <ehowl1117@naver.com>
2024-05-17 16:28:44 +0900

    [css] 로그인 페이지 css 수정

--- moodoodleFront/moodoodlefront/src/components/LoginComponent.jsx
+++ moodoodleFront/moodoodlefront/src/components/LoginComponent.jsx
@@ -17,54 +17,44 @@ export default function LoginComponent() {
   const boxStyle = 'w-[283px] h-[43px] pl-[7px] rounded-[10px] bg-gray-scale-1 border border-darkGray/10 text-[13px]';
 
   return (
-    <div className="w-[338px] h-[297px] rounded-[20px] bg-white shadow-loginShadow">
-      {/* 로그인 폼 입력란 */}
-      <div>
-        <div>
-          {/* 아이디 입력란 */}
-          <div className="flex justify-center items-center mt-[30px]">
-            <label className={textStyle} htmlFor="username">
-              아이디
-            </label>
-            <br />
+    <div className="flex justify-center items-center w-[338px] h-[297px] rounded-[20px] bg-white shadow-loginShadow">
+      <div className="flex flex-col h-[250px] items-center justify-between">
+        <div className="flex flex-col h-[140px] justify-between items-center">
+          <div className="flex flex-col h-[61px] justify-between">
+            <p className={textStyle}>&nbsp; 아이디</p>
+            <div className="flex justify-center items-center">
+              <input
+                className={boxStyle}
+                type="text"
+                id="id"
+                value={id}
+                onChange={updateLoginFormData}
+                onReset={setLoginData}
+                placeholder="아이디를 입력하세요"
+              />
+            </div>
           </div>
-          <div className="flex justify-center items-center mb-[10px]">
-            <input
-              className={boxStyle}
-              type="text"
-              id="id"
-              value={id}
-              onChange={updateLoginFormData}
-              onReset={setLoginData}
-              placeholder="아이디를 입력하세요"
-            />
-            <br />
+          <div className="flex flex-col h-[61px] justify-between">
+            <p className={textStyle}>&nbsp; 비밀번호</p>
+            <div className="flex justify-center items-center mb-[10px]">
+              <input
+                className={boxStyle}
+                type="password"
+                id="password"
+                value={password}
+                onChange={updateLoginFormData}
+                onReset={setLoginData}
+                placeholder="비밀번호를 입력하세요"
+              />
+            </div>
           </div>
-          {/* 비밀번호 입력란 */}
-          <div className="flex justify-center items-center">
-            <label className={textStyle} htmlFor="password">
-              비밀번호
-            </label>
-          </div>
-          <div className="flex justify-center items-center mb-[10px]">
-            <input
-              className={boxStyle}
-              type="password"
-              id="password"
-              value={password}
-              onChange={updateLoginFormData}
-              onReset={setLoginData}
-              placeholder="비밀번호를 입력하세요"
-            />
-          </div>
-          <br />
         </div>
-        {loading && <label className="text-red">아이디 혹은 비밀번호가 틀렸습니다.</label>}
-        {/* 로그인 버튼 */}
-        <div className="flex justify-center items-center mb-[10px]">
-          <LoginButton text="로그인" onClick={() => login({ id, password })} />
+        <div className="flex h-[13px] justify-center items-center">
+          <p className={`text-center text-[13px] text-red-600 ${loading ? '' : 'hidden'}`}>
+            아이디 혹은 비밀번호가 틀렸습니다.
+          </p>
         </div>
-        {/* 회원가입 이동 문구 */}
+        <LoginButton text="로그인" onClick={() => login({ id, password })} />
         <div className="flex justify-center items-center">
           <p className="text-xs text-left text-darkGray mr-[13px]">아직 회원이 아니신가요?</p>
           <Link to="/signup" className="text-[13px] font-bold text-lightBlue">
