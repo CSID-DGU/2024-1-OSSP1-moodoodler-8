7738bdf39db929a48d38965dc0aeeaaf0fbc9f10
teom142 <xodn9245@naver.com>
2024-05-28 14:40:45 +0900

    Feat : add music recomand api

--- moodoodleBack/moodoodle/music/urls.py
+++ moodoodleBack/moodoodle/music/urls.py
@@ -1,9 +1,10 @@
 # music urls.py
 from django.urls import path
-from .views import MusicCreateView
+from .views import MusicCreateView, MusicMoodView
 
 app_name ='music'
 
 urlpatterns = [
-    path('create/', MusicCreateView.as_view(), name='music')
+    path('create/', MusicCreateView.as_view(), name='music'),
+    path('recomand/<int:diary_id>/', MusicMoodView.as_view(), name='music_mood')
 ]
