50b0b939339049c2a3a3ba49f2e5223f10704c39
jisu <jisukim924@gmail.com>
2024-06-04 03:00:05 +0900

    [design] 친구 달력 디자인

--- moodoodleFront/moodoodlefront/src/components/FriendCalendar.jsx
+++ moodoodleFront/moodoodlefront/src/components/FriendCalendar.jsx
@@ -8,21 +8,29 @@ export default function FriendCalendar({
   setSelectedDate,
   handleShowCalendar,
 }) {
+  const style = {
+    transform: 'scale(0.9)',
+    transformOrigin: 'center', // 크기 조절 기준점을 중심으로 설정
+    width: '100%',
+    height: '100%',
+  };
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='flex flex-col items-center w-[342px] h-[370px] relative rounded-[20px] bg-white'>
-        <div className='flex justify-between w-[300px] h-[40px]'>
-          <p className='self-end w-full font-bold text-center text-darkNavy'>{to_user.nickname}님의 감정 달력</p>
+    <div className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-30'>
+      <div className='flex flex-col items-center w-[342px] h-[370px] relative rounded-[20px] gap-[10px] bg-white'>
+        <div className='flex justify-between w-[280px] h-[80px]'>
+          <p className='self-end w-full font-extrabold text-center text-darkNavy'>{to_user.nickname}님의 감정 달력</p>
           <button className='self-end cursor-pointer' type='button' onClick={handleShowCalendar}>
             <img src='/assets/close.svg' alt='닫기' />
           </button>
         </div>
-        <Calendar
-          handleColorChipToggle={handleColorChipToggle}
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-          to_user_id={to_user.id}
-        />
+        <div style={style}>
+          <Calendar
+            handleColorChipToggle={handleColorChipToggle}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            to_user_id={to_user.id}
+          />
+        </div>
       </div>
     </div>
   );
