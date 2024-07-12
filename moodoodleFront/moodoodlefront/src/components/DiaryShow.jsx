740c5034225ed0c4effca11219b07722fb158f02
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-07-12 15:12:38 +0900

    [refactr] 일기 내용 컴포넌트 css 수정

--- moodoodleFront/moodoodlefront/src/components/DiaryShow.jsx
+++ moodoodleFront/moodoodlefront/src/components/DiaryShow.jsx
@@ -19,8 +19,8 @@ export default function DiaryShow({
   const { handleDeleteDiary } = useDiaryWrite();
 
   return (
-    <div className='relative flex justify-center items-center w-[342px] h-[300px] rounded-[20px] bg-white shadow-componentShadow'>
-      <div className='flex flex-col justify-between items-center w-[175px] h-[250px]'>
+    <div className='relative flex justify-center items-center w-[342px] h-[380px] rounded-[20px] bg-white shadow-componentShadow'>
+      <div className='flex flex-col justify-between items-center w-[175px] h-[330px]'>
         <div className='flex flex-col h-[46px] justify-between items-center'>
           <p className='font-bold text-base text-darkNavy'>나의 감정 일기</p>
           <p className='font-medium text-sm text-darkGray'>
@@ -37,6 +37,7 @@ export default function DiaryShow({
           />
         ) : (
           <DiaryContent
+            className='w-[298px]'
             diary_id={diary_id}
             content={content}
             text={text}
